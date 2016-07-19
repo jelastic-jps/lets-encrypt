7c8c112d26478e9c6cd5250ac1fa55b7cc891f00
siruslan <siruslan@gmail.com>
2016-07-19 21:38:21 +0200

    Update generate-ssl-cert.sh

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -32,17 +32,20 @@ appdomain=$(cut -d"." -f2- <<< $appdomain)
 
 echo appid = $appid
 echo appdomain = $appdomain
+
 #Upload 3 certificate files
 uploadresult=$(curl -F "appid=$appid" -F "fid=privkey.pem" -F "file=@$certdir/privkey.pem" -F "fid=fullchain.pem" -F "file=@$certdir/fullchain.pem" -F "fid=cert.pem" -F "file=@$certdir/cert.pem" http://app.$appdomain/xssu/rest/upload)
+#Upload 3 certificate files one by file
+#echo $(curl -F "appid=$appid" -F "fid=privkey.pem" -F "file=@$certdir/privkey.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/privkey.url
+#echo $(curl -F "appid=$appid" -F "fid=fullchain.pem" -F "file=@$certdir/fullchain.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/fullchain.url
+#echo $(curl -F "appid=$appid" -F "fid=cert.pem" -F "file=@$certdir/cert.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/cert.url
 
 #Save urls to certificate files
 echo $uploadresult | awk -F '{"file":"' '{print $2}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/privkey.url
 echo $uploadresult | awk -F '{"file":"' '{print $3}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/fullchain.url
 echo $uploadresult | awk -F '{"file":"' '{print $4}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' > /tmp/cert.url
 
-#Upload 3 certificate files one by file
-#echo $(curl -F "appid=$appid" -F "fid=privkey.pem" -F "file=@$certdir/privkey.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/privkey.url
-#echo $(curl -F "appid=$appid" -F "fid=fullchain.pem" -F "file=@$certdir/fullchain.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/fullchain.url
-#echo $(curl -F "appid=$appid" -F "fid=cert.pem" -F "file=@$certdir/cert.pem" http://app.$appdomain/xssu/rest/upload | awk -F ",\"" '{print $2}' | awk -F ":\"" '{print $2}' | sed 's/"//g') >> /tmp/cert.url
-
-
+#installing ssl cert via JEM
+sed -i '/function doDownloadKeys/a return 0;#letsenctemp' /usr/lib/jelastic/modules/keystore.module
+jem ssl install
+sed -i  '/letsenctemp/d' /usr/lib/jelastic/modules/keystore.module
