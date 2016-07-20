ffb848a6b03f22f31d2ce3c283f6af8ce98c490b
Jelastic JPS Collection <ik+1@jelastic.com>
2016-07-20 18:10:11 +0300

    Update generate-ssl-cert.sh

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -13,14 +13,14 @@ cd /opt/letsencrypt
 git pull origin master
 
 iptables -I INPUT -p tcp -m tcp --dport 9999 -j ACCEPT
-iptables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 9999
+iptables -t nat -I PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 9999
 
-#Request for certificates - breaks certs
-./letsencrypt-auto certonly --standalone --test-cert --break-my-certs $domain --standalone-supported-challenges http-01 --http-01-port 9999 --renew-by-default --email $email --agree-tos
+#Request for certificates - test certs
+./letsencrypt-auto certonly --standalone --test-cert --break-my-certs --domain $domain --standalone-supported-challenges tls-sni-01 --tls-sni-01-port 9999 --renew-by-default --email $email --agree-tos
 #Request for certificates - valid certs
-#./letsencrypt-auto certonly --standalone $domain --standalone-supported-challenges http-01 --http-01-port 9999 --renew-by-default --email $email --agree-tos
+#./letsencrypt-auto certonly --standalone --domain $domain --standalone-supported-challenges tls-sni-01 --tls-sni-01-port 9999 --renew-by-default --email $email --agree-tos
 
-iptables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 9999
+iptables -t nat -D PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 9999
 iptables -D INPUT -p tcp -m tcp --dport 9999 -j ACCEPT
 
 certdir=$(sed -nr '/^[[:digit:]-]{10} [[:digit:]:]{8},[[:digit:]]+:INFO:[[:alnum:]\.]*:Reporting to user: Congratulations![[:alnum:][:space:]]*([^[:blank:]]+)[/][^/[[:blank:]]+[.][[:alnum:][:space:]]*.*$/{s//\1/p}' /var/log/letsencrypt/letsencrypt.log | tail -n 1)
