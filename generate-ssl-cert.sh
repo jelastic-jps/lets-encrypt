d1c64597a28fc3c2352ca95957e82e042ddbe0ee
Jelastic JPS Collection <ik+1@jelastic.com>
2016-07-26 14:43:45 +0300

    Update generate-ssl-cert.sh

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -16,7 +16,7 @@ iptables -I INPUT -p tcp -m tcp --dport 9999 -j ACCEPT
 iptables -t nat -I PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 9999
 
 #Request for certificates - test certs
-./letsencrypt-auto certonly --standalone --test-cert --break-my-certs --domains $domain --standalone-supported-challenges tls-sni-01 --tls-sni-01-port 9999 --renew-by-default --email $email --agree-tos
+./letsencrypt-auto certonly --standalone --test-cert --break-my-certs $domain --standalone-supported-challenges tls-sni-01 --tls-sni-01-port 9999 --renew-by-default --email $email --agree-tos
 #Request for certificates - valid certs
 #./letsencrypt-auto certonly --standalone --domain $domain --standalone-supported-challenges tls-sni-01 --tls-sni-01-port 9999 --renew-by-default --email $email --agree-tos
 
