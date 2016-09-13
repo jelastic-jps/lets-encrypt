59221a909a6b8276d679703d506393393a8a7a4b
ihorman <ihorman@gmail.com>
2016-09-13 10:26:32 +0200

    Update generate-ssl-cert.sh

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -24,11 +24,20 @@ iptables -t nat -I PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 9
 iptables -t nat -D PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 9999
 iptables -D INPUT -p tcp -m tcp --dport 9999 -j ACCEPT
 
-certdir=$(sed -nr '/^[[:digit:]-]{10} [[:digit:]:]{8},[[:digit:]]+:INFO:[[:alnum:]\.]*:Reporting to user: Congratulations![[:alnum:][:space:]]*([^[:blank:]]+)[/][^/[[:blank:]]+[.][[:alnum:][:space:]]*.*$/{s//\1/p}' /var/log/letsencrypt/letsencrypt.log | tail -n 1)
+#To be sure that r/w access
+mkdir -p /tmp/
+chmod -R 777 /tmp/
+appdomain=$(cut -d"." -f2- <<< $appdomain)
 
-echo "------\n"
-echo $certdir
-echo "\n------"
+echo appid = $appid
+echo appdomain = $appdomain
+#Upload 3 certificate files
+uploadresult=$(curl -F "appid=$appid" -F "fid=privkey.pem" -F "file=@$certdir/privkey.pem" -F "fid=fullchain.pem" -F "file=@$certdir/fullchain.pem" -F "fid=cert.pem" -F "file=@$certdir/cert.pem" http://app.$appdomain/xssu/rest/upload)
+
+#Save urls to certificate files
+echo $uploadresult | awk -F '{"file":"' '{print $2}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' >> /tmp/privkey.url
+echo $uploadresult | awk -F '{"file":"' '{print $3}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' >> /tmp/fullchain.url
+echo $uploadresult | awk -F '{"file":"' '{print $4}' | awk -F ":\"" '{print $1}' | sed 's/","name"//g' >> /tmp/cert.url
 
 #installing ssl cert via JEM
 #sed -i '/function doDownloadKeys/a return 0;#letsenctemp' /usr/lib/jelastic/modules/keystore.module
