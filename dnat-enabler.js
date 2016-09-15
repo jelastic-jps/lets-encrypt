2ca6b6a84d4cb7768c22e452f758f490795a55f4
ihorman <ihorman@gmail.com>
2016-09-15 21:15:55 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -14,7 +14,7 @@ for (var i = 1, n = nodes.length; i < n; i++) {
     groups[group] = [];
     groups[group].push(nodes[i].nodeGroup)
 }
-print('test');
+print(groups[group]);
 
 if (groups['lb'] ) selected = groups['lb'] 
     else if (groups['bl'] )  selected = groups['bl'] 
