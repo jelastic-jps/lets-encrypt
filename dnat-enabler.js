89414cba446263a7baaa5abe1c9d0b00bac0a90b
ihorman <ihorman@gmail.com>
2016-09-16 01:22:42 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -19,10 +19,9 @@ if(selectedGroupLenght == 1) return {result: 0, responses: "alone node in entry 
        
 for (var i = 0, n = nodes.length; i < n; i++) { 
       
-print (i);
-      
       if(nodes[i].nodeGroup != selectedGroup) continue;
       if(!masterIP){ masterIP = nodes[i].address; continue; }
+print (masterIP);
 
       if (action == 'add') {
             dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443',
