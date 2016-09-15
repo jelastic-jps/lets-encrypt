fe627864ad387546cbc8be50c0d4a5eed19af727
ihorman <ihorman@gmail.com>
2016-09-15 16:21:25 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -5,11 +5,11 @@ nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
 enabled = getParam("enabled"),
 IPs = [], resp = [];
 
-for (var i = 0; i < nodes.length; i++) { 
+for (var i = 0, n = nodes.length; i < n;) { 
       if (enabled) 
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, [ { "command": "echo", "params": "snat-enabled >> /root/test" } ], true, "root"));;
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "echo", "params": "snat-enabled >> /root/test" } ]), true, "root"));;
       else 
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, "echo snat-disabled >> /root/test", true, "root"));;
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "echo", "params": "snat-disabled >> /root/test" } ]), true, "root"));;
 }
 
 
