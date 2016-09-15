36e8f38eaed108beb6fc3461677050109d22a45e
ihorman <ihorman@gmail.com>
2016-09-15 18:32:14 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -4,14 +4,16 @@ var envName = '${env.envName}',
 nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
 enabled = Boolean(getParam("enabled")),
 masterIP = nodes[0].address,
+iptablesDnatStringEnable = 'enableForIp' + masterIP + '>> /root/test',
+iptablesDnatStringDisable = 'disableForIp' + masterIP + '>> /root/test',
 IPs = [], resp = [];
 
 for (var i = 1, n = nodes.length; i < n; i++) { 
 
     if (enabled) {
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "echo", "params": "snat-enabled-for"+masterIP" >> /root/test" } ]), true, "root"));; 
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "echo", "params": iptablesDnatStringEnable } ]), true, "root"));; 
     } else {
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "echo", "params": "snat-disabled-for"+masterIP" >> /root/test" } ]), true, "root"));;
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "echo", "params": iptablesDnatStringDisable } ]), true, "root"));;
  
     }
 }
