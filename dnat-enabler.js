f0f6e772457fabbe553392a04b11dee756a3ec5f
ihorman <ihorman@gmail.com>
2016-09-15 19:27:06 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -4,16 +4,28 @@ var envName = '${env.envName}',
 nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
 enabled = Boolean(getParam("enabled")),
 masterIP = nodes[0].address,
-iptablesDnatStringEnable = 'enableForIp' + masterIP + '>> /root/test',
-iptablesDnatStringDisable = 'disableForIp' + masterIP + '>> /root/test',
+dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
+dnatDisableParams = ' -t nat -D PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
+groups = {}, 
+selected,
 IPs = [], resp = [];
 
 for (var i = 1, n = nodes.length; i < n; i++) { 
+   if (!groups[group]) 
+    groups[group] = [];
+    groups[group].push(nodes[i].nodeGroup)
+}
+
+if (groups[‘lb’] ) selected = groups[‘lb’] 
+    else if (groups[‘bl’] )  selected = groups[‘bl’] 
+            else selected = groups[‘cp’]
+
+for (var i = 1, n = selected.length; i < n; i++) { 
 
     if (enabled) {
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "echo", "params": iptablesDnatStringEnable } ]), true, "root"));; 
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, selected[i].id,  toJSON( [ { "command": "iptables", "params": dnatEnableParams } ]), true, "root"));; 
     } else {
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "echo", "params": iptablesDnatStringDisable } ]), true, "root"));;
+            resp.push(jelastic.env.control.ExecCmdById(envName, session, selected[i].id, toJSON( [ { "command": "iptables", "params": dnatDisableParams } ]), true, "root"));;
  
     }
 }
