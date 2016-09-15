8a31903c183ff127e13f32ac520cdb90e8406e2a
ihorman <ihorman@gmail.com>
2016-09-15 18:05:07 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -7,7 +7,7 @@ enabled = getParam("enabled"),
 IPs = [], resp = [];
 
 for (var i = 0, n = nodes.length; i < n; i++) { 
-    if (true) 
+    if (enabled) 
             resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "echo", "params": "snat-enabled >> /root/test" } ]), true, "root")); 
         else 
             resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "echo", "params": "snat-disabled >> /root/test" } ]), true, "root"));
