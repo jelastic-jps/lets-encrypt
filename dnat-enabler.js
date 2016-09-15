7f96ff488ba2022829f624fa4628268f39270d39
ihorman <ihorman@gmail.com>
2016-09-15 23:45:13 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -4,8 +4,6 @@ var envName = '${env.envName}',
 nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
 enabled = Boolean(getParam("enabled")),
 masterIP = nodes[0].address,
-dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
-dnatDisableParams = ' -t nat -D PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
 groups = [], selectedGroup, selectedGroupLenght
 IPs = [], resp = [];
 
@@ -13,32 +11,28 @@ for (var i = 0, n = nodes.length; i < n; i++) {
       groups.push(nodes[i].nodeGroup)
     }
     
-if(groups.toString().indexOf("bl") > -1) { selectedGroup = 'bl'; selectedGroupLenght = '${nodes.bl.length}'; }
+if(groups.toString().indexOf("bl") > -1) { selectedGroup = 'bl';selectedGroupLenght = '${nodes.bl.length}'; }
    else if(groups.toString().indexOf("lb") > -1) { selectedGroup = 'lb'; selectedGroupLenght = '${nodes.lb.length}'; }
       else { selectedGroup ='cp' ; selectedGroupLenght = '${nodes.lb.length}'; }
+
+if(selectedGroupLenght == 1) exit;
        
-/* if (groups['lb'] ) selected = groups['lb'] 
-    else if (groups['bl'] )  selected = groups['bl'] 
-            else selected = groups['cp']
-            
-*/
-
-//var xxxx = '${nodes.' + '.cp.' + '.length}';
-print (selectedGroupLenght);
-
-//for (var i = 1, n = nodes[selectedGroup].length; i < n; i++) { 
-
-//print (nodes[selectedGroup].length);
-
+for (var i = 0, n = nodes.length; i < n; i++) { 
+      if(nodes[i].nodeGroup != selectedGroup) continue;
+      if(!masterIP) masterIP = nodes[i].address;
+      print (masterIP);
 
 /* 
     if (enabled) {
+            dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443',
             resp.push(jelastic.env.control.ExecCmdById(envName, session, selected[i].id,  toJSON( [ { "command": "iptables", "params": dnatEnableParams } ]), true, "root"));; 
     } else {
+            dnatDisableParams = ' -t nat -D PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443',
             resp.push(jelastic.env.control.ExecCmdById(envName, session, selected[i].id, toJSON( [ { "command": "iptables", "params": dnatDisableParams } ]), true, "root"));;
- 
     }
-} */
+*/
+
+} 
 
 
 return {
