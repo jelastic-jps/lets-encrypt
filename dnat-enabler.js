4e7c099ed69905441f6f54abe0d27cad1813bb69
ihorman <ihorman@gmail.com>
2016-09-15 21:58:08 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -6,21 +6,24 @@ enabled = Boolean(getParam("enabled")),
 masterIP = nodes[0].address,
 dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
 dnatDisableParams = ' -t nat -D PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP,
-groups = {}, group, selected = {},
+groups = [], selectedGroup = {},
 IPs = [], resp = [];
 
-for (var i = 1, n = nodes.length; i < n; i++) { 
-   if (!groups[group]) 
-    groups[group] = [];
-    groups[group].push(nodes[i])
-}
-print (groups[cp]);
+for (var i = 0, n = nodes.length; i < n; i++) { 
+      groups.push(nodes[i].nodeGroup)
+    }
+    
+if(contains.call(groups, "bl") selectedGroup ='lb'
+   else if(contains.call(groups, "lb") selectedGroup ='lb'
+      else  selectedGroup ='lb'
 
-if (groups['lb'] ) selected = groups['lb'] 
+print (selectedGroup);
+
+/*if (groups['lb'] ) selected = groups['lb'] 
     else if (groups['bl'] )  selected = groups['bl'] 
             else selected = groups['cp']
             
-
+/*
 /*for (var i = 1, n = selected.length; i < n; i++) { 
 
     if (enabled) {
