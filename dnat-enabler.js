cdaeda2fdf1b5ce0ac9b9f4597e0e4d2a2741216
ihorman <ihorman@gmail.com>
2016-09-16 10:43:17 +0200

    Update dnat-enabler.js

--- dnat-enabler.js
+++ dnat-enabler.js
@@ -1,36 +1,32 @@
 //@auth
+//@req(action)
 
 var envName = '${env.envName}', 
-nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes,
-action = getParam("action"), masterIP,
-groups = [], selectedGroup, selectedGroupLenght,
-dnatEnableParams,dnatDisableParams,
-IPs = [], resp = [];
+nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes, 
+masterIP, groupsMap = {}, resp = [];
 
-for (var i = 0, n = nodes.length; i < n; i++) { 
-      groups.push(nodes[i].nodeGroup)
-    }
-    
-if(groups.toString().indexOf("bl") > -1) { selectedGroup = 'bl';selectedGroupLenght = '${nodes.bl.length}'; }
-   else if(groups.toString().indexOf("lb") > -1) { selectedGroup = 'lb'; selectedGroupLenght = '${nodes.lb.length}'; }
-      else { selectedGroup ='cp' ; selectedGroupLenght = '${nodes.lb.length}'; }
+for (var i = 0, n = nodes.length; i < n; i++) {
+      var ng = nodes[i].nodeGroup;
+      if (!groupsMap[ng]) groupsMap[ng] = []; 
+      groupsMap[ng].push(nodes[i]);
+}
 
-if(selectedGroupLenght == 1) return {result: 0, responses: "alone node in entry point layer" };
+var layerNodes = groupsMap['lb'] ? groupsMap['lb'] : (groupsMap['bl'] ? groupsMap['bl'] : groupsMap['cp']);
 
-masterIP='';       
-for (var i = 0, n = nodes.length; i < n; i++) { 
-      
-      if(nodes[i].nodeGroup != selectedGroup) continue
-      if(!masterIP){ masterIP = nodes[i].address; continue }
-print (masterIP);
+if (layerNodes.length == 1) {
+      return {result: 0, responses: "one node in entry point layer" };      
+}
 
-      if (action == 'add') {
-            dnatEnableParams = ' -t nat -I PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443',
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id,  toJSON( [ { "command": "iptables", "params": dnatEnableParams } ]), true, "root"));; 
-      } else {
-            dnatDisableParams = ' -t nat -D PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443',
-            resp.push(jelastic.env.control.ExecCmdById(envName, session, nodes[i].id, toJSON( [ { "command": "iptables", "params": dnatDisableParams } ]), true, "root"));;
+for (var i = 0, n = layerNodes.length; i < n; i++) { 
+      if(layerNodes[i].ismaster) {
+            masterIP = layerNodes[i].address;
+            break;
       }
+}
+
+for (var i = 0, n = layerNodes.length; i < n; i++) { 
+      var dnatParams = ' -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
+      resp.push(jelastic.env.control.ExecCmdById(envName, session, layerNodes[i].id,  toJSON( [ { "command": "iptables", "params": dnatParams } ]), true, "root"));; 
 } 
 
 return {
