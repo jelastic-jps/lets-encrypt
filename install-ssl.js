2b832acab59de769272ac8f09a41adc1091682e7
sych74 <sych74@gmail.com>
2016-09-28 19:55:31 +0200

    Update install-ssl.js

--- install-ssl.js
+++ install-ssl.js
@@ -10,7 +10,9 @@ var envDomain = getParam("domain") || "${ENV_DOMAIN}";
 //var session = jelastic.system.admin.SigninAsUser(appid, signature, '${USER_EMAIL}').session;
 //var testresp = jelastic.env.control.GetEnvInfo(envName, session);
 //
-var testresp = jelastic.env.control.GetEnvInfo("58bdf83fea6af021e0c94ba13730fd6b/${ENV_APPID}", session);
+//var testresp = jelastic.env.control.GetEnvInfo("58bdf83fea6af021e0c94ba13730fd6b/${ENV_APPID}", session);
+var testresp = jelastic.env.control.GetEnvInfo("58bdf83fea6af021e0c94ba13730fd6b/370029fbcf808b89d6f2628b2da0004c", session);
+
 
 
 print (testresp);
