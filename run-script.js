7f4f8154575baf0092c6b654d293f1f2f0742be8
ihorman <ihorman@gmail.com>
2016-09-28 16:44:23 +0200

    Update run-script.js

--- run-script.js
+++ run-script.js
@@ -1,15 +1,18 @@
-//@url('/updatessl') 
+//@url('/updatessl')
 //@required('token', 'targetAppid', 'url')
 
 import com.hivext.api.core.utils.Transport;
 import com.hivext.api.utils.Random;
-import com.hivext.dev.scripting.Eval;
 
 var scriptBody = new Transport().get(url);
-
+var params = {
+    urlLeScript : "https://raw.githubusercontent.com/jelastic-jps/lets-encrypt/master/install-le.sh",
+    urlGenScript : "https://raw.githubusercontent.com/jelastic-jps/lets-encrypt/master/generate-ssl-cert.sh"
+}
 
 if (token == "${TOKEN}") {
-  return hivext.dev.scripting.Eval(targetAppid, session, scriptBody, params)
+  return hivext.dev.scripting.EvalCode(appid, signature, scriptBody, "js", null, params)
 } else {
   return {"result": 8, "error": "wrong token"}
 }
+
