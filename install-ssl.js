f332fcd84203cb0c3a60a040ec668e61201c6633
Ruslan Synytsky <siruslan@gmail.com>
2016-09-29 11:10:18 +0200

    Update install-ssl.js

--- install-ssl.js
+++ install-ssl.js
@@ -7,17 +7,6 @@ if (token != "${TOKEN}") {
 }
 
 var envDomain = getParam("domain") || "${ENV_DOMAIN}";
-//var session = jelastic.system.admin.SigninAsUser(appid, signature, '${USER_EMAIL}').session;
-//var testresp = jelastic.env.control.GetEnvInfo(envName, session);
-//
-//var testresp = jelastic.env.control.GetEnvInfo("58bdf83fea6af021e0c94ba13730fd6b/${ENV_APPID}", session); //58bdf83fea6af021e0c94ba13730fd6b
-var testresp = jelastic.env.control.GetEnvInfo("${ENV_APPID}/58bdf83fea6af021e0c94ba13730fd6b", signature);
-
-
-
-print (testresp);
-
-return 0;
 
 var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes, 
 masterIP, masterID, groupsMap = {}, resp = [], envDomain;
