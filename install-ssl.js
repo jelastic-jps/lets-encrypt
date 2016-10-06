51d0a90684a682738791a380af46659c26a26ce3
sych74 <sych74@gmail.com>
2016-10-06 17:33:47 +0200

    Update install-ssl.js

--- install-ssl.js
+++ install-ssl.js
@@ -1,38 +1,22 @@
 //auth
 //@url('/install-ssl-script')
-//@req(token, envName, urlLeScript, urlGenScript, group)
+//@req(token)
 
 if (token != "${TOKEN}") {
   return {"result": 8, "error": "wrong token"}
 }
 
-var envDomain = getParam("domain") || "${ENV_DOMAIN}";
-var masterId = getParam("masterId") || "${MASTER_ID}"
+var envDomain = getParam("domain") || "${ENV_DOMAIN}",
+envName = getParam("envName") || "${ENV_NAME}",
+masterId = getParam("masterId") || "${MASTER_ID}",
+masterIP = getParam("masterIP") || "${MASTER_IP}",
+urlLeScript = getParam("urlLeScript") || "${LE_INSTALL}",
+urlGenScript = getParam("urlGenScript") || "${LE_GENERATE_SSL}",   
+group = getParam("group") || "${NODE_GROUP}",
+email = getParam("email") || "${USER_EMAIL}",
+envAppid = getParam("envAppid") || "${ENV_APPID}";
 
-/*
-var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes, 
-masterIP, masterID, groupsMap = {}, resp = [], envDomain;
 
-/*
-for (var i = 0, n = nodes.length; i < n; i++) {
-      var ng = nodes[i].nodeGroup;
-      if (!groupsMap[ng]) groupsMap[ng] = []; 
-      groupsMap[ng].push(nodes[i]);
-}
-
-var layerNodes = groupsMap['lb'] ? groupsMap['lb'] : (groupsMap['bl'] ? groupsMap['bl'] : groupsMap['cp']);
-
-if (layerNodes.length == 1) {
-      return {result: 0, responses: "one node in entry point layer" };      
-}
-
-/*
-for (var i = 0, n = layerNodes.length; i < n; i++) { 
-      if(!layerNodes[i].extIPs) {
-        jelastic.env.control.AttachExtIp(envName, signature, layerNodes[i].id);
-      }
-}
-*/
 
 function manageDnat(action)
   var dnatParams = ' -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
@@ -49,7 +33,7 @@ var execParamsGe = ' ' + urlGenScript + ' -O /root/generate-ssl-cert.sh && chmod
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterId,  toJSON( [ { "command": "wget", "params": execParamsGe } ]), true, "root"));; 
 
 //exec
-var createSettingsParams = '\"domain=\''+envDomain+'\' \n email=\'${USER_EMAIL}\' \n appid=\'${ENV_APPID}\' \n appdomain=\'${ENV_DOMAIN}\'\" >  /opt/letsencrypt/settings' 
+var createSettingsParams = '\"domain=\''+envDomain+'\' \n email=\''+email+'\' \n appid=\''+envAppid+'\' \n appdomain=\''+envDomain+'\'\" >  /opt/letsencrypt/settings' 
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterId,  toJSON( [ { "command": "printf", "params": createSettingsParams } ]), true, "root"));; 
 var execParamsMain = '/root/generate-ssl-cert.sh'
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterId,  toJSON( [ { "command": "bash", "params": execParamsMain } ]), true, "root"));; 
