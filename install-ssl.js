c0502805e0f8caee77e905f764f033b47ee0bbb8
sych74 <sych74@gmail.com>
2016-10-06 16:59:03 +0200

    Update install-ssl.js

--- install-ssl.js
+++ install-ssl.js
@@ -1,6 +1,6 @@
 //auth
 //@url('/install-ssl-script')
-//@req(token, envName, urlLeScript, urlGenScript)
+//@req(token, envName, urlLeScript, urlGenScript, group)
 
 if (token != "${TOKEN}") {
   return {"result": 8, "error": "wrong token"}
@@ -8,10 +8,11 @@ if (token != "${TOKEN}") {
 
 var envDomain = getParam("domain") || "${ENV_DOMAIN}";
 
+/*
 var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes, 
 masterIP, masterID, groupsMap = {}, resp = [], envDomain;
 
-
+/*
 for (var i = 0, n = nodes.length; i < n; i++) {
       var ng = nodes[i].nodeGroup;
       if (!groupsMap[ng]) groupsMap[ng] = []; 
@@ -24,39 +25,36 @@ if (layerNodes.length == 1) {
       return {result: 0, responses: "one node in entry point layer" };      
 }
 
+/*
 for (var i = 0, n = layerNodes.length; i < n; i++) { 
       if(!layerNodes[i].extIPs) {
         jelastic.env.control.AttachExtIp(envName, signature, layerNodes[i].id);
       }
-      if(layerNodes[i].ismaster) {
-            masterIP = layerNodes[i].address;
-            masterID = layerNodes[i].id
-            break;
-      }
 }
+*/
 
 function manageDnat(action)
- {
-      for (var i = 0, n = layerNodes.length; i < n; i++) { 
-            if(layerNodes[i].ismaster) continue;
-            var dnatParams = ' -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
-            resp.push(jelastic.env.control.ExecCmdById(envName, signature, layerNodes[i].id,  toJSON( [ { "command": "iptables", "params": dnatParams } ]), true, "root"));; 
-      } 
-
- }
+  var dnatParams = ' -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
+  resp.push(jelastic.env.control.ExecCmdByGroup(envName, signature, group,  toJSON( [ { "command": "iptables", "params": dnatParams } ]), true, "root"));; 
+}
 
 manageDnat('add');
 
+
+//download 
 var execParamsLe = ' ' + urlLeScript + ' -O /root/install-le.sh && chmod +x /root/install-le.sh && /root/install-le.sh >> /var/log/letsencrypt.log';
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterID,  toJSON( [ { "command": "wget", "params": execParamsLe } ]), true, "root"));; 
 var execParamsGe = ' ' + urlGenScript + ' -O /root/generate-ssl-cert.sh && chmod +x /root/generate-ssl-cert.sh';
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterID,  toJSON( [ { "command": "wget", "params": execParamsGe } ]), true, "root"));; 
+
+//exec
 var createSettingsParams = '\"domain=\''+envDomain+'\' \n email=\'${USER_EMAIL}\' \n appid=\'${ENV_APPID}\' \n appdomain=\'${ENV_DOMAIN}\'\" >  /opt/letsencrypt/settings' 
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterID,  toJSON( [ { "command": "printf", "params": createSettingsParams } ]), true, "root"));; 
 var execParamsMain = '/root/generate-ssl-cert.sh'
 resp.push(jelastic.env.control.ExecCmdById(envName, signature, masterID,  toJSON( [ { "command": "bash", "params": execParamsMain } ]), true, "root"));; 
+
 //read certificates
-var cert_key = jelastic.env.file.Read(envName, signature, "/tmp/privkey.url", null, null,masterID);
+var cert_key = jelastic.env.file.Read(envName, signature, "/tmp/privkey.url", null, null, masterID);
 var fullchain = jelastic.env.file.Read(envName, signature, "/tmp/fullchain.url", null, null, masterID);
 var cert = jelastic.env.file.Read(envName, signature, "/tmp/cert.url", null, null, masterID);
 
