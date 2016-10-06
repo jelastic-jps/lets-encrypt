f88c4507195819b96324223b1d1c4ffc90a6281e
sych74 <sych74@gmail.com>
2016-10-06 17:33:51 +0200

    Update upload-custom-script.js

--- upload-custom-script.js
+++ upload-custom-script.js
@@ -1,28 +1,9 @@
 //@auth
-//@required('url', 'scriptName', 'scriptType')
+//@required('url', 'scriptName', 'scriptType', urlLeScript, urlGenScript)
 
 import com.hivext.api.core.utils.Transport;
 import com.hivext.api.utils.Random;
 
-//reading script from URL
-var scriptBody = new Transport().get(url);
-
-//inject token
-var token = Random.getPswd(64);
-scriptBody = scriptBody.replace("${TOKEN}", token);
-scriptBody = scriptBody.replace("${ENV_DOMAIN}", "${env.domain}");
-scriptBody = scriptBody.replace("${USER_EMAIL}", "${user.email}");
-scriptBody = scriptBody.replace("${ENV_APPID}", "${env.appid}");
-
-//delete the script if it exists already
-jelastic.dev.scripting.DeleteScript(scriptName);
-
-//create a new script 
-var resp = hivext.dev.scripting.CreateScript(scriptName, scriptType, scriptBody);
-if (resp.result != 0) return resp;
-
-//get app domain
-var domain = jelastic.dev.apps.GetApp(appid).hosting.domain;
 
 //get nodeGroup 
 var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes, 
@@ -34,6 +15,43 @@ for (var i = 0, n = nodes.length; i < n; i++) {
           break;
       }
 }
+
+var masterId, masterIP;
+for (var i = 0, n = nodes.length; i < n; i++) {
+      if (nodes[i].nodeGroup != group) continue;
+      if(!nodes[i].extIPs) jelastic.env.control.AttachExtIp(envName, session, nodes[i].id);
+      if(nodes[i].ismaster) { 
+            masterId = nodes[i].id;
+            masterIP = nodes[i].address;
+      }
+}
+
+//reading script from URL
+var scriptBody = new Transport().get(url);
+
+//inject token
+var token = Random.getPswd(64);
+scriptBody = scriptBody.replace("${TOKEN}", token);
+scriptBody = scriptBody.replace("${ENV_DOMAIN}", "${env.domain}");
+scriptBody = scriptBody.replace("${USER_EMAIL}", "${user.email}");
+scriptBody = scriptBody.replace("${ENV_APPID}", "${env.appid}");
+scriptBody = scriptBody.replace("${ENV_NAME}", '${env.envName}');
+scriptBody = scriptBody.replace("${LE_INSTALL}", urlLeScript);
+scriptBody = scriptBody.replace("${LE_GENERATE_SSL}", urlGenScript);
+scriptBody = scriptBody.replace("${NODE_GROUP}", group);
+scriptBody = scriptBody.replace("${MASTER_IP}", masterIP);
+scriptBody = scriptBody.replace("${MASTER_ID}", masterId);
+
+
+//delete the script if it exists already
+jelastic.dev.scripting.DeleteScript(scriptName);
+
+//create a new script 
+var resp = hivext.dev.scripting.CreateScript(scriptName, scriptType, scriptBody);
+if (resp.result != 0) return resp;
+
+//get app domain
+var domain = jelastic.dev.apps.GetApp(appid).hosting.domain;
 
 //eval the script 
 var resp = hivext.dev.scripting.Eval(scriptName, {
