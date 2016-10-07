74085e12bf2b7f0535018bf94bf5417d671c169d
ihorman <ihorman@gmail.com>
2016-10-07 13:37:53 +0200

    Update install-ssl.js

--- install-ssl.js
+++ install-ssl.js
@@ -24,29 +24,19 @@ function manageDnat(action) {
 
 manageDnat('add');
 
-println(resp)
-
 //download 
 var execParamsLe = ' ' + urlLeScript + ' -O /root/install-le.sh && chmod +x /root/install-le.sh && /root/install-le.sh >> /var/log/letsencrypt.log';
 resp = jelastic.env.control.ExecCmdById(envAppid, session, masterId,  toJSON( [ { "command": "wget", "params": execParamsLe } ]), true, "root"); 
 
-println(resp)
-
 var execParamsGe = ' ' + urlGenScript + ' -O /root/generate-ssl-cert.sh && chmod +x /root/generate-ssl-cert.sh';
 resp = jelastic.env.control.ExecCmdById(envName, session, masterId,  toJSON( [ { "command": "wget", "params": execParamsGe } ]), true, "root"); 
-
-println(resp)
 
 //exec
 var createSettingsParams = '\"domain=\''+envDomain+'\' \n email=\''+email+'\' \n appid=\''+envAppid+'\' \n appdomain=\''+envDomain+'\'\" >  /opt/letsencrypt/settings' 
 resp = jelastic.env.control.ExecCmdById(envName, session, masterId,  toJSON( [ { "command": "printf", "params": createSettingsParams } ]), true, "root"); 
 
-println(resp)
-
 var execParamsMain = '/root/generate-ssl-cert.sh'
 resp = jelastic.env.control.ExecCmdById(envName, session, masterId,  toJSON( [ { "command": "bash", "params": execParamsMain } ]), true, "root"); 
-
-println(resp)
 
 //read certificates
 var cert_key = jelastic.env.file.Read(envName, session, "/tmp/privkey.url", null, null, masterId);
