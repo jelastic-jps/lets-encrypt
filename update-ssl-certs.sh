5929bccb99bdf967fc8c5c817f5b117d8e1f3c6c
ihorman <ihorman@gmail.com>
2016-10-21 15:18:55 +0200

    Update update-ssl-certs.sh

--- update-ssl-certs.sh
+++ update-ssl-certs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-DAYS_BEFORE_EXPIRE=14
+DAYS_BEFORE_EXPIRE=1400
 
-env_domain=$1
-env_name=$2
-token=$3
+cs_appid=$1
+token=$2
 seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
 wget=$(which wget);
 exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
@@ -14,5 +13,5 @@ _cur_date_unixtime=$(date "+%s");
 _delta_time=$(( $_exp_date_unixtime - $_cur_date_unixtime  ));
 [[ $_delta_time -le $seconds_before_expire ]] && {
     echo "$(date) - update required" >> /var/log/letsencrypt.log;
-    wget -qO- "http://${env_domain}/${env_name}-install-ssl-script?token=$token"
+    wget -qO- "http://${cs_appid}.$(hostname | sed "s/$(hostname | cut -d'.' -f1))//g"/${env_name}-install-ssl-script?token=$token"
 }
