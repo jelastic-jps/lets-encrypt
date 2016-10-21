ad6713a2273183a7070f9b543cc133fb090eadbd
ihorman <ihorman@gmail.com>
2016-10-21 15:57:06 +0200

    Update update-ssl-certs.sh

--- update-ssl-certs.sh
+++ update-ssl-certs.sh
@@ -8,7 +8,7 @@ token=$3
 seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
 wget=$(which wget);
 exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
-[ -z exp_date  ] && echo "no certificates for update";
+[ -z exp_date  ] && { echo "no certificates for update"; exit 0; };
 _exp_date_unixtime=$(date --date="$exp_date" "+%s");
 _cur_date_unixtime=$(date "+%s");
 _delta_time=$(( $_exp_date_unixtime - $_cur_date_unixtime  ));
