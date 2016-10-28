6f79bf1a53975231b61ecdd4c75b33431adab0c4
sych74 <sych74@gmail.com>
2016-10-28 09:41:10 +0300

    Update and rename update-ssl-certs.sh to auto-update-cronjob.sh

--- scripts/auto-update-cronjob.sh
+++ scripts/auto-update-cronjob.sh
@@ -2,9 +2,8 @@
 
 DAYS_BEFORE_EXPIRE=14
 
-cs_appid=$1
-env_name=$2
-token=$3
+url=$1
+
 seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
 wget=$(which wget);
 exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
@@ -14,6 +13,5 @@ _cur_date_unixtime=$(date "+%s");
 _delta_time=$(( $_exp_date_unixtime - $_cur_date_unixtime  ));
 [[ $_delta_time -le $seconds_before_expire ]] && {
     echo "$(date) - update required" >> /var/log/letsencrypt.log;
-    cs_domain="${cs_appid}.app$(hostname | sed "s/$(hostname | cut -d'.' -f1)//g")";
-    wget -qO- "http://${cs_domain}/${env_name}-install-ssl-script?token=$token"
+    wget -qO- ${url}
 }
