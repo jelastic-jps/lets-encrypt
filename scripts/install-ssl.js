f3ce7652fb22361c25b7fb72bd3952460acbc820
sych74 <sych74@gmail.com>
2016-10-29 13:16:44 +0300

    Update install-ssl.js

--- scripts/install-ssl.js
+++ scripts/install-ssl.js
@@ -25,14 +25,6 @@ resp, debug = [];
 //multi domain support - any following separator can be used: ' ' or ';' or ',' 
 if (customDomain) customDomain = customDomain.split(";").join(" ").split(",").join(" ").replace(/\s+/g, " ").trim().split(" ").join(" -d ");
 
-function manageDnat(action) {
-  var dnatParams = 'a | grep -q  ' + masterIP + ' || iptables -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
-  resp = jelastic.env.control.ExecCmdByGroup(envName, session, group, toJSON([{ "command": "ip", "params": dnatParams }]), true, false, "root"); 
-  debug.push(resp);
-}
-
-manageDnat('add');
-
 //download and execute Let's Encrypt package installation script 
 var fileName = urlLeScript.split('/').pop().split('?').shift();
 var execParams = ' ' + urlLeScript + ' -O /root/' + fileName + ' && chmod +x /root/' + fileName + ' && /root/' + fileName + ' >> /var/log/letsencrypt.log';
@@ -50,10 +42,28 @@ execParams = '\"domain=\'' + (customDomain || envDomain) + '\'\nemail=\''+email+
 resp = jelastic.env.control.ExecCmdById(envName, session, masterId,  toJSON( [ { "command": "printf", "params": execParams } ]), true, "root"); 
 debug.push(resp);
 
+manageDnat('add');
+
 //execute SSL generation script 
 execParams = '/root/' + fileName;
 resp = jelastic.env.control.ExecCmdById(envName, session, masterId,  toJSON( [ { "command": "bash", "params": execParams } ]), true, "root"); 
 debug.push(resp);
+
+manageDnat('remove');
+
+//checking errors of the SSL generation process 
+var out = resp.responses[0].out;
+var ind1 = out.indexOf("Reporting to user: The following errors");
+if (ind1 != -1){
+  var ind2 = out.indexOf("appid =", ind1);
+  var error = ind2 == -1 ? out.substring(ind1) : out.substring(ind1, ind2);
+  return {
+    result: 99,
+    error: error,
+    response: error,
+    debug: debug
+  }
+}
 
 //download and configure cron job for auto update script 
 var autoUpdateUrl = getParam('autoUpdateUrl');
@@ -67,26 +77,27 @@ if (autoUpdateUrl) {
 
 //read certificates
 var cert_key = jelastic.env.file.Read(envName, session, "/tmp/privkey.url", null, null, masterId);
-var fullchain = jelastic.env.file.Read(envName, session, "/tmp/fullchain.url", null, null, masterId);
 var cert = jelastic.env.file.Read(envName, session, "/tmp/cert.url", null, null, masterId);
+var fullchain = jelastic.env.file.Read(envName, session, "/tmp/fullchain.url", null, null, masterId);
 
-manageDnat('remove');
-
-resp = jelastic.env.binder.BindSSL(envName, session, cert_key.body, cert.body, fullchain.body);
-debug.push(resp);
-
-
-return {
-   result : resp.result,
-   response: debug.join('\n'), 
-   onAfterReturn : {
-      call : {
-         procedure: 'log',
-         params: {
-            message: debug
-         }
-      }
-   }
+if (cert_key.body && fullchain.body && cert.body){
+  resp = jelastic.env.binder.BindSSL(envName, session, cert_key.body, cert.body, fullchain.body);
+  debug.push(resp);
+} else {
+  var error = "can't read ssl certificate";
+  resp = {
+    result: 99, 
+    error: error,
+    response: error
+  }
 }
 
+resp.debug = debug;
+return resp;
+
+function manageDnat(action) {
+  var dnatParams = 'a | grep -q  ' + masterIP + ' || iptables -t nat ' + (action == 'add' ? '-I' : '-D') + ' PREROUTING -p tcp --dport 443 -j DNAT --to-destination ' + masterIP + ':443';
+  resp = jelastic.env.control.ExecCmdByGroup(envName, session, group, toJSON([{ "command": "ip", "params": dnatParams }]), true, false, "root"); 
+  debug.push(resp);
+}
 
