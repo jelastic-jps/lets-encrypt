875b2750ce0d8cba4a079fbd9fe72fe4a0a3ddb7
ihorman <ihorman@gmail.com>
2016-12-08 14:01:16 +0100

    Update install-ssl.js

--- scripts/install-ssl.js
+++ scripts/install-ssl.js
@@ -70,7 +70,7 @@ resp = ExecCmdById("wget", execParams);
 debug.push(resp);
 
 //write configs for ssl generation
-var primarydDomain = window.location.host;
+var primaryDomain = window.location.host;
 execParams = '\"domain=\'' + (customDomain || envDomain) + '\'\nemail=\''+email+'\'\nappid=\''+envAppid+'\'\nappdomain=\''+envDomain+'\'\ntest=\''+ (customDomain ? false : true)+  '\'\nprimarydomain=\''+primaryDomain +  '\'\n\" >  /opt/letsencrypt/settings' 
 resp = ExecCmdById("printf", execParams); 
 debug.push(resp);
