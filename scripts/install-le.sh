3542b5212c26fc10428320219ad12808f5dca62a
ihorman <ihorman@gmail.com>
2017-03-13 17:05:25 +0200

    Update install-le.sh

--- scripts/install-le.sh
+++ scripts/install-le.sh
@@ -7,9 +7,8 @@ rpm -qa > /dev/null 2>&1  || rpm --rebuilddb
 echo Install opel-release
 yum -y install epel-release git bc;
 
-rpm -ivh https://downloads.hpdd.intel.com/public/e2fsprogs/1.42.12.wc1/el7/RPMS/x86_64/libcom_err-devel-1.42.12.wc1-4.el7.centos.x86_64.rpm;
-
-git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
+rpm -qa  | grep -q libcom_err || rpm -ivh https://downloads.hpdd.intel.com/public/e2fsprogs/1.42.12.wc1/el7/RPMS/x86_64/libcom_err-devel-1.42.12.wc1-4.el7.centos.x86_64.rpm;
+[ ! -d '/opt/letsencrypt' ] && git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
 
 /opt/letsencrypt/letsencrypt-auto --os-packages-only
 
