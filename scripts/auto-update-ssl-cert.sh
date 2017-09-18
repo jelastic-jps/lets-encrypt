ba946352ff840eb572173b89acbac56036b21cbf
ihorman <ihorman@gmail.com>
2017-09-18 16:02:37 +0300

    Update auto-update-ssl-cert.sh

--- scripts/auto-update-ssl-cert.sh
+++ scripts/auto-update-ssl-cert.sh
@@ -10,7 +10,7 @@ auto_update_url=$1
 seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
 wget=$(which wget);
 [ -f "/var/lib/jelastic/SSL/jelastic.crt" ] && exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
-[ -z $exp_date  ] && { echo "$(date) - no certificates for update" >> /var/log/letsencrypt.log; exit 0; };
+[ -z "$exp_date" ] && { echo "$(date) - no certificates for update" >> /var/log/letsencrypt.log; exit 0; };
 _exp_date_unixtime=$(date --date="$exp_date" "+%s");
 _cur_date_unixtime=$(date "+%s");
 _delta_time=$(( $_exp_date_unixtime - $_cur_date_unixtime  ));
