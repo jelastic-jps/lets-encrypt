5610d21d58212c08af76b5560f8de9e4fadcfaa0
ihorman <ihorman@gmail.com>
2017-10-09 17:45:40 +0300

    RPM installation fixes
    related to issue that caused 4101 error in JEL

--- scripts/install-le.sh
+++ scripts/install-le.sh
@@ -2,16 +2,18 @@
 
 echo Checking RPM database 
 
-rpm -qa > /dev/null 2>&1  || rpm --rebuilddb
+{
+  rpm -qa > /dev/null 2>&1  || rpm --rebuilddb
+} &> /dev/null
 
-echo Install opel-release
-yum -y install epel-release git bc;
 
-rpm -qa  | grep -q libcom_err || rpm -ivh https://downloads.hpdd.intel.com/public/e2fsprogs/1.42.12.wc1/el7/RPMS/x86_64/libcom_err-devel-1.42.12.wc1-4.el7.centos.x86_64.rpm;
-[ ! -d '/opt/letsencrypt' ] && git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
+echo "Installing required packages"
+{
+  yum -y install epel-release git bc;
+  [ ! -d '/opt/letsencrypt' ] && git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
+  /opt/letsencrypt/letsencrypt-auto --os-packages-only
 
-/opt/letsencrypt/letsencrypt-auto --os-packages-only
-
+} &> /dev/null
 
 JEM_SSL_MODULE_LATEST_URL="https://raw.githubusercontent.com/jelastic/jem/master/usr/lib/jelastic/modules/ssl.module"
 JEM_SSL_MODULE_PATH="/usr/lib/jelastic/modules/ssl.module"
