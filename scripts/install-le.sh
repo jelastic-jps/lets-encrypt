049a13770e2c9efc8a5cd07025f16ba8f869f73e
ihorman <ihorman@gmail.com>
2017-11-02 17:35:26 +0200

    Update install-le.sh

--- scripts/install-le.sh
+++ scripts/install-le.sh
@@ -2,19 +2,22 @@
 
 echo Checking RPM database 
 
-rpm -qa > /dev/null 2>&1  || rpm --rebuilddb
+{
+  rpm -qa > /dev/null 2>&1  || rpm --rebuilddb
+} &> /dev/null
 
-echo Install opel-release
-yum -y install epel-release git bc;
 
-rpm -qa  | grep -q libcom_err || rpm -ivh https://downloads.hpdd.intel.com/public/e2fsprogs/1.42.12.wc1/el7/RPMS/x86_64/libcom_err-devel-1.42.12.wc1-4.el7.centos.x86_64.rpm;
-[ ! -d '/opt/letsencrypt' ] && git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
+echo "Installing required packages"
+{
+  yum -y install epel-release git bc;
+  [ ! -d '/opt/letsencrypt' ] && git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt;
+  /opt/letsencrypt/letsencrypt-auto --os-packages-only
 
-/opt/letsencrypt/letsencrypt-auto --os-packages-only
-
+} &> /dev/null
 
 JEM_SSL_MODULE_LATEST_URL="https://raw.githubusercontent.com/jelastic/jem/master/usr/lib/jelastic/modules/ssl.module"
 JEM_SSL_MODULE_PATH="/usr/lib/jelastic/modules/ssl.module"
 localedef -i en_US -f UTF-8 en_US.UTF-8
 wget --no-check-certificate "https://raw.githubusercontent.com/jelastic/jem/master/usr/lib/jelastic/modules/ssl.module" -O $JEM_SSL_MODULE_PATH
-[ -d "/var/www/" ] && chown -R apache:apache "/var/www/" 2> /dev/null
+[ -d "/var/www/" ] && chown -R 700:700 "/var/www/" 2> /dev/null
+exit 0
