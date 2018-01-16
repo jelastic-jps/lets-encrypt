d26c1ccddc200db6d441774f1010d38ab0193ecd
Ihor Kolodyuk <ik@jelastic.com>
2018-01-16 10:54:06 +0200

    multiple-ip-bub is merged to trunk

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -1,4 +1,6 @@
-EXT_IP=$(ip r get 1 | awk '{print $5}' | head -n 1)
+#!/bin/bash
+
+EXT_IPs=$(/usr/sbin/ip a | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
 function isLANIP() {
     : ${1:?"Missing param: IP"};
@@ -13,26 +15,33 @@ function isLANIP() {
 }
 
 function validateExtIP(){
-        isLANIP $EXT_IP || return 0 && { echo "Error: External IP is required!"; exit 1; }
+    for EXT_IP in $EXT_IPs
+    do
+            isLANIP $EXT_IP || return 0;
+    done
+    { echo "Error: External IP is required!"; exit 1; }
 }
 
 function validateDNSSettings(){
-	domain=$1;
-	[ -z "$domain" ] && { 
-		[ -f '/opt/letsencrypt/settings'  ] && source '/opt/letsencrypt/settings' || { echo "Error: no settings available" ; exit 3 ; }
-	}
+    domain=$1;
+    [ -z "$domain" ] && {
+        [ -f '/opt/letsencrypt/settings'  ] && source '/opt/letsencrypt/settings' || { echo "Error: no settings available" ; exit 3 ; }
+    }
 
-	domain_list=$(echo $domain | sed "s/,/ /g")
-    	for single_domain in $domain_list
-    	do
-		[ "$single_domain" == "-d" ] && continue;
-    		dig +short  @8.8.8.8 A $single_domain | grep -q $EXT_IP  || { echo "Error: Incorrect DNS settings for domain $single_domain! It should be bound to $EXT_IP."; exit 1; };
-    	done
-    	return 0
+    domain_list=$(echo $domain | sed "s/,/ /g")
+        for single_domain in $domain_list
+        do
+        [ "$single_domain" == "-d" ] && continue;
+        for EXT_IP in $EXT_IPs
+        do
+                dig +short  @8.8.8.8 A $single_domain | grep -q $EXT_IP && return 0;
+        done
+        done
+    { echo "Error: Incorrect DNS settings for domain $single_domain! It should be bound to $EXT_IP."; exit 1 ; };
 }
 
 function validateCertBot(){
-	[ -f "/opt/letsencrypt/certbot-auto" ] && return 0  || { echo "Error: Certbot is not installed!"; exit 1 ; };
+    [ -f "/opt/letsencrypt/certbot-auto" ] && return 0  || { echo "Error: Certbot is not installed!"; exit 1 ; };
 }
 
 function runAllChecks(){
