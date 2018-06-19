c5b0378fd34f463eefedc3a3ef61482072c5b332
ihorman <ihorman@gmail.com>
2018-06-19 23:15:49 +0300

    autoupdate stuff is added

--- scripts/auto-update-ssl-cert.sh
+++ scripts/auto-update-ssl-cert.sh
@@ -6,6 +6,9 @@ WGET=$(which wget);
 OPENSSL=$(which openssl)
 GREP=$(which grep)
 SED=$(which sed)
+GIT=$(which git);
+BASE_REPO_URL="https://github.com/jelastic-jps/lets-encrypt"
+RAW_REPO_SCRIPS_URL="https://raw.githubusercontent.com/jelastic-jps/lets-encrypt/multiple-ip-bug/scripts/"
 
 [ -f "${DIR}/opt/letsencrypt/settings"  ] && source "${DIR}/opt/letsencrypt/settings" || { echo "No settings available" ; exit 3 ; }
 [ -f "${DIR}/root/validation.sh"  ] && source "${DIR}/root/validation.sh" || { echo "No validation library available" ; exit 3 ; }
@@ -28,6 +31,27 @@ seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
     exp_date=$(date --utc --date="$exp_date_raw" "+%Y-%m-%d %H:%M:%S");
 }
 
+function validateLatestVersion(){
+   local revision_state_path="/root/.lerevision";
+   local latest_revision=$($GIT ls-remote $BASE_REPO_URL | grep master | awk '{ print $1}');
+   [ -f "$revision_state_path" ] && current_revision=$(cat $revision_state_path);
+   [ "$latest_revision" != "$current_revision" ] && {
+        $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/auto-update-ssl-cert.sh -O /tmp/auto-update-ssl-cert.sh
+        $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/install-le.sh -O /tmp/install-le.sh
+        $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/validation.sh -O /tmp/validation.sh
+        $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/generate-ssl-cert.sh -O /tmp/generate-ssl-cert.sh
+        [  -s /tmp/auto-update-ssl-cert.sh -a -s /tmp/install-le.sh -a -s /tmp/validation.sh -a -s /tmp/generate-ssl-cert.sh ] && {
+            mv /tmp/install-le.sh /root/install-le.sh
+            mv /tmp/auto-update-ssl-cert.sh  /root/auto-update-ssl-cert.sh
+            mv /tmp/generate-ssl-cert.sh /root/generate-ssl-cert.sh
+            mv /tmp/validation.sh /root/validation.sh
+            chmod +x /root/*.sh
+            echo $latest_revision > $revision_state_path
+        }
+   }
+}
+
+
 [ -z "$exp_date" ] && { echo "$(date) - no certificates for update" >> /var/log/letsencrypt.log; exit 0; };
 
 _exp_date_unixtime=$(date --date="$exp_date" "+%s");
@@ -36,6 +60,7 @@ _delta_time=$(( $_exp_date_unixtime - $_cur_date_unixtime  ));
 
 [[ $_delta_time -le $seconds_before_expire ]] && {
     echo "$(date) - update required" >> /var/log/letsencrypt.log;
+    validateLatestVersion
     resp=$($WGET --no-check-certificate -qO- "${auto_update_url}");
     { echo "${resp#*response*}" | sed 's/"//g' | grep -q 'result:0' ;} || $WGET -qO- "${jerror_url}/jerror?appid=$appid&actionname=updatefromcontainer&callparameters=$auto_update_url&email=$email&errorcode=4121&errormessage=$resp&priority=high"
 }
