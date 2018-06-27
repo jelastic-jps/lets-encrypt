cdb032543a750e7142ba22b1206ec7e11cbedea1
ihorman <ihorman@gmail.com>
2018-06-27 12:41:31 +0300

    Backwards compartiblity tweak

--- scripts/auto-update-ssl-cert.sh
+++ scripts/auto-update-ssl-cert.sh
@@ -25,7 +25,7 @@ jerror_url=$(awk -F "/" '{ print $1"//"$2$3"/1.0/environment/jerror/rest"}' <<< 
 seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
 
 
-#[ -f "${DIR}var/lib/jelastic/SSL/jelastic.crt" ] && exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
+[ -f "/var/lib/jelastic/SSL/jelastic.crt" ] && exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
 [ -f "${DIR}/var/lib/jelastic/cert.pem" ] && {
     exp_date_raw=$($OPENSSL x509 -text -noout -in "${DIR}/var/lib/jelastic/keys/cert.pem" -subject -enddate | $GREP notAfter | $SED 's/^notAfter=//');
     exp_date=$(date --utc --date="$exp_date_raw" "+%Y-%m-%d %H:%M:%S");
