d4bdbfb6cc14f58961438f2b18c567767802d697
Joakim Hindersson <joakim.hindersson@elastx.se>
2018-07-04 12:42:56 +0200

    Fixed cronjob to parse environment variables before execution and ipv6 validation of domain

--- scripts/validation.sh
+++ scripts/validation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 IP=$(which ip)
+[[ -z "$IP" ]] && { echo "ip command not found, unable to verify IP"; exit 3 ; }
+
 EXT_IPs=$($IP a | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+EXT_IPs_v6=$($IP a | sed -En 's/inet6 ::1\/128//;s/.*inet6 (addr:?)?([0-9a-f:]+)\/.*/\2/p')
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 
 function isLANIP() {
@@ -16,11 +19,29 @@ function isLANIP() {
     return 1
 }
 
+function isLANIPv6() {
+    : ${1:?"Missing param: IP"};
+    local ip
+    local pip=${1}
+    OIFS=$IFS;IFS=':';ip=(${pip});IFS=$OIFS
+    [[ ${ip[0]} == "fc00" ]] && return 0;
+    [[ ${ip[0]} == "fe80" ]] && return 0;
+    [[ ${ip[0]} == "ff0[0-9a-f]" ]] && return 0;
+    [[ ${ip[0]} == "2002" ]] && return 0;
+    [[ ${ip[0]} == "2001" && ${ip[1]} == "db8" ]] && return 0;
+    return 1
+}
+
 function validateExtIP(){
     for EXT_IP in $EXT_IPs
     do
-            isLANIP $EXT_IP || return 0;
+            isLANIP $EXT_IP || HAS_EXTERNAL_v4=0;
     done
+    for EXT_IP in $EXT_IPs_v6
+    do
+            isLANIPv6 $EXT_IP || HAS_EXTERNAL_v6=0;
+    done
+    [[ $HAS_EXTERNAL_v4 -eq 0 || $HAS_EXTERNAL_v6 -eq 0 ]] && return 0;
     { echo "Error: External IP is required!"; exit 1; }
 }
 
@@ -39,9 +60,13 @@ function validateDNSSettings(){
             do
                 dig +short @8.8.8.8 A $single_domain | grep -q $EXT_IP && detected=true;
             done
+            for EXT_IP in $EXT_IPs_v6
+            do
+                dig +short @8.8.8.8 AAAA $single_domain | grep -q $EXT_IP && detected=true;
+            done
 	    [[ $detected == 'false'  ]] && { echo "Error: Incorrect DNS settings for domain $single_domain! It should be bound to containers Public IP."; exit 1 ; };
         done
-	    return 0;
+        return 0;
 }
 
 function validateCertBot(){
