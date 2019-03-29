2f08f5d3f6213356979100c821a1f35794519a8d
alexey.lazarenko <dzotic9@gmail.com>
2019-03-29 17:34:02 +0200

    JE-42350

--- scripts/auto-update-ssl-cert.sh
+++ scripts/auto-update-ssl-cert.sh
@@ -12,29 +12,6 @@ RAW_REPO_SCRIPS_URL="https://raw.githubusercontent.com/jelastic-jps/lets-encrypt
 
 [[ -z "$WGET" || -z "$OPENSSL" || -z "$GREP" || -z "$SED" || -z "$GIT" ]] && { echo "PATH not set with neccessary commands"; exit 3 ; }
 
-updateScripts
-
-[ -f "${DIR}/opt/letsencrypt/settings"  ] && source "${DIR}/opt/letsencrypt/settings" || { echo "No settings available" ; exit 3 ; }
-[ -f "${DIR}/root/validation.sh"  ] && source "${DIR}/root/validation.sh" || { echo "No validation library available" ; exit 3 ; }
-
-validateExtIP
-validateDNSSettings
-
-TIME_TO_WAIT=$(($RANDOM%3600));
-#sleep $TIME_TO_WAIT;   
-
-auto_update_url=$1
-jerror_url=$(awk -F "/" '{ print $1"//"$2$3"/1.0/environment/jerror/rest"}' <<< $auto_update_url )
-
-seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
-
-[ -f "${DIR}/var/lib/jelastic/keys/cert.pem" ] && {
-    exp_date_raw=$($OPENSSL x509 -text -noout -in "${DIR}/var/lib/jelastic/keys/cert.pem" -subject -enddate | $GREP notAfter | $SED 's/^notAfter=//');
-    exp_date=$(date --utc --date="$exp_date_raw" "+%Y-%m-%d %H:%M:%S");
-}
-
-[ -f "/var/lib/jelastic/SSL/jelastic.crt" ] && exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
-
 function validateLatestVersion(){
    local revision_state_path="/root/.lerevision";
    local latest_revision=$($GIT ls-remote $BASE_REPO_URL | grep master | awk '{ print $1}');
@@ -58,6 +35,29 @@ function updateScripts(){
     $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/validation.sh -O /tmp/validation.sh
     $WGET  --no-check-certificate $RAW_REPO_SCRIPS_URL/generate-ssl-cert.sh -O /tmp/generate-ssl-cert.sh
 }
+
+updateScripts
+
+[ -f "${DIR}/opt/letsencrypt/settings"  ] && source "${DIR}/opt/letsencrypt/settings" || { echo "No settings available" ; exit 3 ; }
+[ -f "${DIR}/root/validation.sh"  ] && source "${DIR}/root/validation.sh" || { echo "No validation library available" ; exit 3 ; }
+
+validateExtIP
+validateDNSSettings
+
+TIME_TO_WAIT=$(($RANDOM%3600));
+#sleep $TIME_TO_WAIT;   
+
+auto_update_url=$1
+jerror_url=$(awk -F "/" '{ print $1"//"$2$3"/1.0/environment/jerror/rest"}' <<< $auto_update_url )
+
+seconds_before_expire=$(( $DAYS_BEFORE_EXPIRE * 24 * 60 * 60 ));
+
+[ -f "${DIR}/var/lib/jelastic/keys/cert.pem" ] && {
+    exp_date_raw=$($OPENSSL x509 -text -noout -in "${DIR}/var/lib/jelastic/keys/cert.pem" -subject -enddate | $GREP notAfter | $SED 's/^notAfter=//');
+    exp_date=$(date --utc --date="$exp_date_raw" "+%Y-%m-%d %H:%M:%S");
+}
+
+[ -f "/var/lib/jelastic/SSL/jelastic.crt" ] && exp_date=$(jem ssl checkdomain | python -c "import sys, json; print json.load(sys.stdin)['expiredate']");
 
 
 [ -z "$exp_date" ] && { echo "$(date) - no certificates for update" >> /var/log/letsencrypt.log; exit 0; };
