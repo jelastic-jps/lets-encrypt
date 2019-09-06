8ec05ca0d48b9f7c0b166e7409ed3d4e5ce6064e
dzotic91-ukr <45935484+dzotic91-ukr@users.noreply.github.com>
2019-09-06 17:28:30 +0300

    Update generate-ssl-cert.sh

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -37,6 +37,7 @@ ip6tables -I INPUT -p tcp -m tcp --dport 12345 -j ACCEPT
 iptables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12345
 ip6tables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12345 || ip6tables -I INPUT -p tcp -m tcp --dport 80 -j DROP
 
+echo "$DIR/opt/letsencrypt/letsencrypt-auto certonly $webroot_params $test_params --domain $domain --preferred-challenges http-01 --http-01-port 12345 --renew-by-default --email $email --agree-tos --logs-dir $DIR/var/log/letsencrypt" >> /tmp/le.log
 #Request for certificates
 $DIR/opt/letsencrypt/letsencrypt-auto certonly $webroot_params $test_params --domain $domain --preferred-challenges http-01 --http-01-port 12345 --renew-by-default --email $email --agree-tos --logs-dir $DIR/var/log/letsencrypt
 
