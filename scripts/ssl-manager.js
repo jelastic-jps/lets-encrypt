3b71bfecd3c7b160eb0a57fb4fb6b366facfe223
alexey.lazarenko <dzotic9@gmail.com>
2019-09-18 13:22:55 +0300

    Update ssl-manager.js

--- scripts/ssl-manager.js
+++ scripts/ssl-manager.js
@@ -89,6 +89,7 @@ function SSLManager(config) {
 
     me.install = function (isUpdate) {
         var resp = me.exec([
+            [ me.initAddOnExtIp ],
             [ me.installLetsEncrypt ],
             [ me.generateSslConfig ],
             [ me.generateSslCerts ],
