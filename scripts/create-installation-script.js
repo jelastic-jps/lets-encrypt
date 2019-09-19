ddfbb8d0ed0d3a402ea389636acf14ed51168c77
alexey.lazarenko <dzotic9@gmail.com>
2019-09-19 15:03:59 +0300

    Update create-installation-script.js

--- scripts/create-installation-script.js
+++ scripts/create-installation-script.js
@@ -12,7 +12,7 @@ var baseDir          = getParam("baseDir", "/"),
     undeployHookType = getParam("undeployHookType", ""),
     withExtIp        = getParam("withExtIp", ""),
     appId            = getParam("appId", "letsencrypt-ssl-addon"),
-    test             = getParam("test", "true");
+    test             = getParam("test", "");
 
 function run() {
     var SSLManager = use("scripts/ssl-manager.js", {
