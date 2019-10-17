e89145f1ded1c85ce382de6ef67a7fe7b5920968
alexey.lazarenko <dzotic9@gmail.com>
2019-10-17 12:13:14 +0300

    added no-bootstrap --no-self-upgrade options

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -34,7 +34,7 @@ iptables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12
 ip6tables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12345 || ip6tables -I INPUT -p tcp -m tcp --dport 80 -j DROP
 
 #Request for certificates
-$DIR/opt/letsencrypt/letsencrypt-auto certonly --standalone $test_params --domain $domain --preferred-challenges http-01 --http-01-port 12345 --renew-by-default --email $email --agree-tos --logs-dir $DIR/var/log/letsencrypt
+$DIR/opt/letsencrypt/letsencrypt-auto certonly --standalone $test_params --domain $domain --preferred-challenges http-01 --http-01-port 12345 --renew-by-default --email $email --agree-tos --no-bootstrap --no-self-upgrade --logs-dir $DIR/var/log/letsencrypt
 
 iptables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12345
 ip6tables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 12345 || ip6tables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
