69f22d70bbaa33ea1ded220b43410e7e6ffd638f
alexey.lazarenko <dzotic9@gmail.com>
2019-11-15 16:56:10 +0200

    Update ssl-manager.js

--- scripts/ssl-manager.js
+++ scripts/ssl-manager.js
@@ -1,5 +1,4 @@
 function SSLManager(config) {
-    import com.hivext.api.environment.response.EnvironmentResponse;
     /**
      * Implements Let's Encrypt SSL management of the Jelastic environment
      * @param {{
@@ -543,7 +542,7 @@ function SSLManager(config) {
     };
 
     me.isBusyExtDomain = function (domain) {
-        var BUSY_RESULT = EnvironmentResponse.ENVIRONMENT_EXT_DOMAIN_IS_BUSY,
+        var BUSY_RESULT = 2330,//EnvironmentResponse.ENVIRONMENT_EXT_DOMAIN_IS_BUSY,
             resp;
 
         resp = jelastic.environment.binder.CheckExtDomain({
