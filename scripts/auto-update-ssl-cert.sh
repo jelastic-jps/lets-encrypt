83b901f7e13746c47e01bf90f641d282a6d6c7d6
alexey.lazarenko <dzotic9@gmail.com>
2020-06-19 15:56:35 +0300

    changed baseUrl

--- scripts/auto-update-ssl-cert.sh
+++ scripts/auto-update-ssl-cert.sh
@@ -8,7 +8,7 @@ GREP=$(which grep)
 SED=$(which sed)
 GIT=$(which git);
 BASE_REPO_URL="https://github.com/jelastic-jps/lets-encrypt"
-RAW_REPO_SCRIPS_URL="https://raw.githubusercontent.com/jelastic-jps/lets-encrypt/master/scripts/"
+RAW_REPO_SCRIPS_URL="https://raw.githubusercontent.com/jelastic-jps/lets-encrypt/stage/scripts/"
 
 [[ -z "$WGET" || -z "$OPENSSL" || -z "$GREP" || -z "$SED" || -z "$GIT" ]] && { echo "PATH not set with neccessary commands"; exit 3 ; }
 
