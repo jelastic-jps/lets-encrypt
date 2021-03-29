5da1bb97ada344134e7972d0861ad031b2a0e3dd
alexey.lazarenko <dzotic9@gmail.com>
2021-03-29 12:16:35 +0300

    JE-56740 LE: replace certbot with acme.sh

--- scripts/install-le.sh
+++ scripts/install-le.sh
@@ -14,14 +14,12 @@ echo "Installing required packages"
 {
   yum-config-manager --save --setopt=pgdg*.skip_if_unavailable=true
   yum -y install epel-release git bc nss;
-  yum -y install tinyproxy --enablerepo='epel';
+  yum -y install tinyproxy socat --enablerepo='epel';
   
   mkdir -p ${DIR}/opt;
-  [ ! -d "${DIR}/opt/letsencrypt" ] && git clone https://github.com/certbot/certbot ${DIR}/opt/letsencrypt;
+  [ ! -d "${DIR}/opt/letsencrypt" ] && git clone https://github.com/acmesh-official/acme.sh ${DIR}/opt/letsencrypt;
   cd $DIR/opt/letsencrypt/
-  git reset --merge
-  git checkout 02a5d000cb1684619650677a2d3fa4972dfd576f
-  ${DIR}/opt/letsencrypt/letsencrypt-auto --os-packages-only
+  ./acme.sh --install --accountemail $email
 }
 
 [ ! -f "${DIR}/root/validation.sh" ] && {
@@ -29,9 +27,9 @@ echo "Installing required packages"
     chmod +x ${DIR}/root/validation.sh
 }
 
-[ -f "/usr/lib/jelastic/modules/ssl.module" ] && {
-    JEM_SSL_MODULE_LATEST_URL="https://raw.githubusercontent.com/jelastic/jem/master/usr/lib/jelastic/modules/ssl.module"
-    JEM_SSL_MODULE_PATH="/usr/lib/jelastic/modules/ssl.module"
+JEM_SSL_MODULE_PATH="/usr/lib/jelastic/modules/ssl.module"
+[[ -f "${JEM_SSL_MODULE_PATH}" && ! -s "$JEM_SSL_MODULE_PATH" ]] && {
+    JEM_SSL_MODULE_LATEST_URL="https://raw.githubusercontent.com/jelastic/jem/master"$JEM_SSL_MODULE_PATH
     localedef -i en_US -f UTF-8 en_US.UTF-8
     wget --no-check-certificate "${JEM_SSL_MODULE_LATEST_URL}" -O $JEM_SSL_MODULE_PATH
 
