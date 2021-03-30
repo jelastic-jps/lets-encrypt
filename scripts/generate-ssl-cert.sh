e6625e9d8c5721053975589aa7b7b4782e223777
alexey.lazarenko <dzotic9@gmail.com>
2021-03-30 11:20:13 +0300

    JE-56740 LE: replace certbot with acme.sh

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -2,6 +2,7 @@
 LOG_FILE=$DIR/var/log/letsencrypt/letsencrypt.log-$(date '+%s')
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..";
 SETTINGS="${DIR}/opt/letsencrypt/settings"
+DOMAIN_SEP=" -d "
 
 [ -f "${SETTINGS}" ] && source "${SETTINGS}" || { echo "No settings available" ; exit 3 ; }
 [ -f "${DIR}/root/validation.sh" ] && source "${DIR}/root/validation.sh" || { echo "No validation library available" ; exit 3 ; }
@@ -24,6 +25,11 @@ params='';
     [[ ! -z ${WEBROOT} ]] && { webrootPath="${WEBROOT}/ROOT/"; } || { echo "Webroot path is not set"; exit 3; }
 }
 [[ "$webroot" == "true" && ! -z "$webrootPath" ]] && { params="--webroot ${webrootPath}"; } || { params=" --standalone --httpport ${LE_PORT} "; }
+
+#format domains list according to acme client
+domain=$(echo $domain | sed -r 's/\s+/ -d /g');
+skipped_domains=$(echo $skipped_domains | sed -r 's/\s+/ -d /g');
+
 [[ ! -z "$skipped_domains" ]] && {
   [[ -z "$domain" ]] && domain=$skipped_domains || domain+=" -d "$skipped_domains;
 }
@@ -45,11 +51,11 @@ mkdir -p $DIR/var/log/letsencrypt
     ip6tables -t nat -I PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports ${LE_PORT} || ip6tables -I INPUT -p tcp -m tcp --dport 80 -j DROP
 }
 result_code=1;
-count=1
+
 while [ "$result_code" != "0" ]
 do
-  [[ -z $domain || $count == 100 ]] && break;
-  ((count=count+1));
+  [[ -z $domain ]] && break;
+
   resp=$($DIR/opt/letsencrypt/acme.sh --issue $params $test_params --domain $domain --nocron -f --log-level 2 --log $LOG_FILE 2>&1)
 
   grep -q 'Cert success' $LOG_FILE && grep -q "BEGIN CERTIFICATE" $LOG_FILE && result_code=0 || result_code=1
@@ -63,7 +69,7 @@ do
     }
 
     all_invalid_domains_errors+=$error";"
-    all_invalid_domains+=$invalid_domain" -d "
+    all_invalid_domains+=$invalid_domain" "
 
     domain=$(echo $domain | sed 's/'${invalid_domain}'\(\s-d\s\)\?//')
     domain=$(echo $domain | sed "s/\s-d$//")
@@ -73,10 +79,12 @@ done
 all_invalid_domains_errors=${all_invalid_domains_errors%?}
 
 [[ ! -z $all_invalid_domains ]] && {
-  all_invalid_domains=$(echo $all_invalid_domains | sed "s/\s-d$//")
+#  all_invalid_domains=$(echo $all_invalid_domains | sed "s/\s-d//gp")
+  all_invalid_domains=$(echo $all_invalid_domains | sed -r "s/\s-d//g")
   sed -i "s|skipped_domains=.*|skipped_domains='${all_invalid_domains}'|g" ${SETTINGS}
 }
-sed -i "s|domain=.*|domain='${domain}'|g" ${SETTINGS}
+domain=$(echo $domain | sed -r "s/\s-d//g");
+sed -i "s|^domain=.*|domain='${domain}'|g" ${SETTINGS};
 
 [[ "$webroot" == "false" ]] && {
     iptables -t nat -D PREROUTING -p tcp -m tcp ! -s 127.0.0.1/32 --dport 80 -j REDIRECT --to-ports ${PROXY_PORT}
