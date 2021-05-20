d6f6536485fc884154044a9cc6c8dd59cf854cff
alexey.lazarenko <dzotic9@gmail.com>
2021-05-20 12:53:00 +0300

    JE-58470 [Let's Encrypt] Support acme.sh v 2.8.9

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -53,10 +53,6 @@ mkdir -p $DIR/var/log/letsencrypt
 }
 result_code=1;
 
-#returning to the old revision - https://github.com/acmesh-official/acme.sh/commit/44615c6fa2115a2010a87ed575699ec8f8a746e8
-cd $DIR/opt/letsencrypt/
-git checkout 2.8.9
-
 while [ "$result_code" != "0" ]
 do
   [[ -z $domain ]] && break;
