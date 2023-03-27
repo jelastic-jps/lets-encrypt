360b9337d8ddea2d0a3d46038b51439136aaecff
Alexey Lazarenko <oleksii.lazarenko@virtuozzo.com>
2023-03-27 17:40:54 +0300

    JE-65967 [LE] Improve le logs while generating SSL certs

--- scripts/generate-ssl-cert.sh
+++ scripts/generate-ssl-cert.sh
@@ -111,8 +111,10 @@ do
       }
     }
 
-    all_invalid_domains_errors+=$error";"
-    all_invalid_domains+=$invalid_domain" "
+    [[ ! -z "$error" ]] && {
+      all_invalid_domains_errors+=$error";";
+      all_invalid_domains+=$invalid_domain" ";
+    }
 
     domain=$(echo $domain | sed 's/'${invalid_domain}'\(\s-d\s\)\?//')
     domain=$(echo $domain | sed "s/\s-d$//")
