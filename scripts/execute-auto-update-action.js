29318524879a6baa0923fbe978a8e0a06af00b70
Slava Katiukha <v.katiukha@gmail.com>
2024-02-29 15:03:54 +0100

    JE-70403 [Let's Encrypt] auto-update issue (#290) (#292)
    JE-70403 [Let's Encrypt] auto-update issue

--- scripts/execute-auto-update-action.js
+++ scripts/execute-auto-update-action.js
@@ -1,23 +1,51 @@
-//@reg(action)
+//@req(action)
 
-var resp = api.dev.scripting.Eval("appstore", session, "GetApps", {
-        targetAppid: "${envAppid}",
-        search: {
-          appstore: 1,
-          app_id: "${appId}",
-          nodeGroup: "${nodeGroup}"
-        }
-      });
+let resp = api.dev.scripting.Eval("appstore", session, "GetApps", {
+    targetAppid: "${envAppid}",
+    search: {
+        appstore: 1,
+        app_id: "${appId}",
+        nodeGroup: "${nodeGroup}"
+    }
+});
 if (resp.result != 0) return resp;
+
 if (resp.response) {
-    if (resp.response.apps && resp.response.apps[0] && resp.response.apps[0].isInstalled) {
-        return api.marketplace.installation.ExecuteAction({
-          appid: appid,
-          session: session,
-          appUniqueName: resp.response.apps[0].uniqueName,
-          action: action
-        });
-    }            
+    let addon = resp.response.apps && resp.response.apps[0];
+
+    if (addon && addon.isInstalled) {
+        let CONFIGURE_ACTION = "configure";
+
+        if (!hasFullCollaborationSupport(addon)) {
+            action = CONFIGURE_ACTION;
+        }
+
+        let actionParams = {
+            appid: appid,
+            session: session,
+            appUniqueName: addon.uniqueName,
+            action: action
+        };
+
+        if (action == CONFIGURE_ACTION) {
+            actionParams.settingsId = "main";
+            actionParams.params = (addon.settings || {}).data || {
+                customDomains: getParam("customDomains") || ""
+            };
+        }
+
+        return api.marketplace.installation.ExecuteAction(actionParams);
+    }
 }
 
-return { result: 0 }
+function hasFullCollaborationSupport(addon) {
+    let resp = api.marketplace.installation.GetInfo({ appUniqueName: addon.uniqueName });
+
+    if (resp.result !== 0) {
+        api.marketplace.console.WriteLog("[WARNING]: api.marketplace.installation.GetInfo = " + resp);
+    }
+
+    return String((resp.app || {}).jps).indexOf("CreatePersistence") > -1;
+}
+
+return { result: 0 };
